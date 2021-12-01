4261fa6645278eef2f62e5a0942386a70cb08d29
JC Salevan <jsalevan@gmail.com>
2021-12-01 00:48:23 -0500

    comments lol

--- AoC1.R
+++ AoC1.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 library(readr)
 
+#haha who needs to remember how to read in text as if it's anything but a csv
 AoC1 = read_csv("AoC1.txt", col_names = F) %>%
   rename(readings = X1) %>%
+  
+  #ain't about to rewrite a rolling mean at midnight
   mutate(rmean = zoo::rollmean(readings, k = 3, fill = NA),
-         d_readings = c(NA, diff(readings)),
+         
+         #doing this a cleaner way is for chumps
+         d_readings = c(NA, diff(readings)), 
          d_rmean = c(NA, diff(rmean)),
          
          sign_d_read = d_readings > 0,
